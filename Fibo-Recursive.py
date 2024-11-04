a5fb05ce294571ef83128417299b79295b6b5cc1
Vedangi-21 <114250307+Vedangi-21@users.noreply.github.com>
2024-11-04 22:38:53 +0530

    Update Fibo-Recursive.py

--- Fibo-Recursive.py
+++ Fibo-Recursive.py
@@ -1,5 +1,3 @@
-
-# Write a recursive program to calculate Fibonacci numbers and find its step count.
 COUNT=0
 def recur_fibo(n):  
    global COUNT
@@ -8,18 +6,14 @@ def recur_fibo(n):
        return n  
    else:  
        return(recur_fibo(n-1) + recur_fibo(n-2))  
-# take input from the user  
-
 
 nterms = int(input("How many terms? "))  
-# check if the number of terms is valid  
 if nterms <= 0:  
    print("Plese enter a positive integer")  
 else:  
    print("Fibonacci sequence:")  
    for i in range(nterms):  
        print(recur_fibo(i))
-
 
 print("Steps reqired using Counter ", COUNT)
 
