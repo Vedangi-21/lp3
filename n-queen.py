a5747afcd17719f0693bfd87f64bf91cc5b32e66
Vedangi-21 <114250307+Vedangi-21@users.noreply.github.com>
2024-11-04 22:45:06 +0530

    Update n-queen.py

--- n-queen.py
+++ n-queen.py
@@ -5,27 +5,22 @@ def print_solution(board):
 
 
 def is_safe(board, row, col):
-    # Check this column on upper side
     for i in range(row):
         if board[i][col]:
             return False
 
-    # Check upper diagonal on left side
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if j < 0:  # Check column bounds
+        if j < 0: 
             break
         if board[i][j]:
             return False
 
-    # Check upper diagonal on right side
     for i, j in zip(range(row, -1, -1), range(col, len(board))):
-        if j >= len(board):  # Check column bounds
+        if j >= len(board):  
             break
         if board[i][j]:
             return False
-
     return True
-
 
 def solve_n_queens_util(board, row):
     if row >= len(board):
@@ -34,17 +29,14 @@ def solve_n_queens_util(board, row):
 
     for col in range(len(board)):
         if is_safe(board, row, col):
-            board[row][col] = True  # Place queen
-            solve_n_queens_util(board, row + 1)  # Recur to place rest of the queens
-            board[row][col] = False  # Backtrack
-
+            board[row][col] = True  
+            solve_n_queens_util(board, row + 1)  
+            board[row][col] = False 
 
 def solve_n_queens(n):
-    board = [[False] * n for _ in range(n)]  # Create an n x n board
+    board = [[False] * n for _ in range(n)] 
     solve_n_queens_util(board, 0)
 
-
-# Main program
 if __name__ == "__main__":
     n = int(input("Enter the number of queens: "))
     solve_n_queens(n)
